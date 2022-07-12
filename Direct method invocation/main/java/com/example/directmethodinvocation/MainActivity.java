89381e9a0589920e931610adde2026a97c2b1ff9
HarshithaSuresh27 <108916769+HarshithaSuresh27@users.noreply.github.com>
2022-07-12 12:38:42 +0530

    Add files via upload

---
+++ Direct method invocation/main/java/com/example/directmethodinvocation/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.directmethodinvocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hello welcome to android class",Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
