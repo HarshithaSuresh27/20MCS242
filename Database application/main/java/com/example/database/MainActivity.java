5be325293483d1dd178f0dfc7bb155beebbe5012
HarshithaSuresh27 <108916769+HarshithaSuresh27@users.noreply.github.com>
2022-09-13 13:27:30 +0530

    Add files via upload

---
+++ Database application/main/java/com/example/database/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.database;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name,contact,dob;
+    Button insert,update,delete,view;
+    DBHelper DB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name=findViewById(R.id.editText);
+        contact=findViewById(R.id.editText1);
+        dob=findViewById(R.id.editText2);
+        insert=findViewById(R.id.btnInsert);
+        update=findViewById(R.id.btnUpdate);
+        delete=findViewById(R.id.btnDelete);
+        view=findViewById(R.id.btnView);
+        DB=new DBHelper(this);
+        insert.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String nameTXT=name.getText().toString();
+                String contactTXT=contact.getText().toString();
+                String dobTXT=dob.getText().toString();
+                Boolean checkinsertdata=DB.insertuserdata(nameTXT,contactTXT,dobTXT);
+                if (checkinsertdata==true)
+                    Toast.makeText(MainActivity.this,"New Entry Inserted",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this,"New Entry not Inserted",Toast.LENGTH_LONG).show();
+            }
+        });
+        update.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String nameTXT=name.getText().toString();
+                String contactTXT=contact.getText().toString();
+                String dobTXT=dob.getText().toString();
+                Boolean checkupdatedata=DB.updateuserdata(nameTXT,contactTXT,dobTXT);
+                if (checkupdatedata==true)
+                    Toast.makeText(MainActivity.this,"Entry Updated",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this,"Entry not Updated",Toast.LENGTH_LONG).show();
+            }
+
+        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String nameTXT=name.getText().toString();
+                Boolean checkdeletedata=DB.deletedata(nameTXT);
+                if (checkdeletedata==true)
+                    Toast.makeText(MainActivity.this,"Entry Deleted",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this,"Entry not Deleted",Toast.LENGTH_LONG).show();
+            }
+        });
+        view.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Cursor res=DB.getdata();
+                if(res.getCount()==0)
+                {
+                    Toast.makeText(MainActivity.this,"No entry found",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                StringBuffer buffer=new StringBuffer();
+                while (res.moveToNext()){
+                    buffer.append("Name: "+res.getString(0)+"\n");
+                    buffer.append("Contact: "+res.getString(1)+"\n");
+                    buffer.append("DOB: "+res.getString(2)+"\n\n");
+                }
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("User Enteries");
+                builder.setMessage(buffer.toString());
+                builder.show();
+            }
+        });
+    }
+}
